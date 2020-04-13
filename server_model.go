99bf2c660f60cd85c441ada646b9659441a02e00
Dof <dev@super-message.com>
2020-04-13 16:09:43 +0800

    使用带下划线的参数

--- server_model.go
+++ server_model.go
@@ -11,8 +11,6 @@ import (
 // QueryParameter 代表用户通过客户端直接向开发者服务器发起请求时 url query 所带的参数
 type QueryParameter struct {
 	RequestToken string
-	// 请求 token 的过期时间戳，用于缓存 token 时设置过期时间，不用每次都向平台发起请求验证 token，避免用户长时间等待
-	TokenExpiredAt int64
 	// 请求是从哪个频道、那条消息、使用哪个版本的模板发起的
 	// 需要注意的是，MessageID 可能是 0，比如用户从菜单中触发的，这将在客户端中生成一条新的消息（平台不会保存）
 	ChannelID       string
@@ -29,27 +27,18 @@ func QueryParameterFrom(r *http.Request) (q *QueryParameter, err error) {
 	q = &QueryParameter{}
 
 	// rt/rte/cid 是一定有的值
-	q.RequestToken = strings.TrimSpace(query.Get("rt"))
+	q.RequestToken = strings.TrimSpace(query.Get("_rt"))
 	if q.RequestToken == "" {
 		return nil, errors.New("request token is required")
 	}
 
-	tokenExpiredAt := query.Get("rte")
-	if tokenExpiredAt == "" {
-		return nil, errors.New("token expiration is required")
-	}
-	q.TokenExpiredAt, err = strconv.ParseInt(tokenExpiredAt, 10, 64)
-	if err != nil {
-		return nil, errors.New("invalid rte value")
-	}
-
-	q.ChannelID = strings.TrimSpace(query.Get("cid"))
+	q.ChannelID = strings.TrimSpace(query.Get("_cid"))
 	if q.ChannelID == "" {
 		return nil, errors.New("channel id not presented")
 	}
 
 	// 下面的参数有传则按类型转换，无传则不管。按需调用其它方法进行检查
-	id := query.Get("id")
+	id := query.Get("_id")
 	if id != "" {
 		q.MessageID, err = strconv.ParseInt(id, 10, 64)
 		if err != nil {
@@ -57,7 +46,7 @@ func QueryParameterFrom(r *http.Request) (q *QueryParameter, err error) {
 		}
 	}
 
-	localID := query.Get("lid")
+	localID := query.Get("_lid")
 	if localID != "" {
 		q.MessageLocalID, err = strconv.ParseInt(localID, 10, 64)
 		if err != nil {
@@ -65,9 +54,9 @@ func QueryParameterFrom(r *http.Request) (q *QueryParameter, err error) {
 		}
 	}
 
-	q.TemplateID = strings.TrimSpace(query.Get("tid"))
+	q.TemplateID = strings.TrimSpace(query.Get("_tid"))
 
-	templateVersion := query.Get("tv")
+	templateVersion := query.Get("_tv")
 	if templateVersion != "" {
 		q.TemplateVersion, err = strconv.Atoi(templateVersion)
 		if err != nil {
